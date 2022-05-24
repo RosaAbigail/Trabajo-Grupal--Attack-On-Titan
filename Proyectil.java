217d1b13330d8f8094933158d520a4997f85a8e7
RosaAbigail <105310834+RosaAbigail@users.noreply.github.com>
2022-05-24 15:27:43 -0300

    Update Proyectil.java

--- Proyectil.java
+++ Proyectil.java
@@ -1,11 +1,10 @@
 package juego;
 
-import java.awt.*;
 import entorno.Entorno;
 import entorno.Herramientas;
+import java.awt.*;
 
 public class Proyectil {
-
 	// Variables
 	private double x;
 	private double y;
@@ -16,44 +15,27 @@ public class Proyectil {
 	// Constructores
 	public Proyectil(Entorno e, double x, double y, double orientacion) {
 		this.x = x;
-	    	this.y = y;
-	    	this.orientacion = orientacion;
-	    	this.velocidad = 5;
-	    	this.radio = 15;
+        this.y = y;
+        this.orientacion = orientacion;
+        this.velocidad = 5;
+        this.radio = 25;
 	}
-
+	
 	// Metodos
 	public void dibujar(Entorno e) {
-		e.dibujarCirculo(x, y, radio, Color.CYAN);
+		e.dibujarCirculo(this.x, this.y, this.radio, Color.CYAN);
 	}
-
 	public void desplazamiento() {
-    		this.x += Math.cos(this.orientacion) * this.velocidad;
-    		this.y += Math.sin(this.orientacion) * this.velocidad;
+        this.x += Math.cos(this.orientacion) * this.velocidad;
+        this.y += Math.sin(this.orientacion) * this.velocidad;
+	}			
+	public boolean colisionConKyojin(Kyojin k) {
+		return (this.x - k.getX()) * (this.x - k.getX()) + (this.y - k.getY()) * (this.y - k.getY()) <= this.radio * k.getRadio();
 	}
-	
-	public boolean colisionConTitan(Titan t) {
-//		double distancia = Math.sqrt ((this.x-t.getX())*(this.x-t.getX()) + (this.y-t.getY())*(this.y-t.getY()));
-//		if (distancia < this.radio + t.getRadio()) {
-//			return true;
-//		}
-//		return false;
-		if (this.x > t.getX() - this.radio/2 && this.x < t.getX() + this.radio/2 && this.y > t.getY() - this.radio/2 && this.y < t.getY() + this.radio/2) {
-			return true;
-		}
-		return false;
-	}
-	
 	public boolean colisionConObstaculo (Obstaculo o) {
-		double distancia = Math.sqrt ((this.x-o.getX())*(this.x-o.getX()) + (this.y-o.getY())*(this.y-o.getY()));
-		if (distancia < this.radio + o.getRadio()) {
-			return true;
-		}
-		return false;
+		return (this.x - o.getX()) * (this.x - o.getX()) + (this.y - o.getY()) * (this.y - o.getY()) <= this.radio * o.getRadio();
 	}
-	
 	public boolean colisionConEntorno(Entorno e) {
-		return this.x < this.radio/2 || this.x > e.ancho() - this.radio/2 || this.y < this.radio/2 || this.y > e.alto() - this.radio/2;
+		return this.x <= this.radio || this.y <= this.radio || this.x >= e.ancho() - this.radio || this.y >= e.alto() - this.radio;
 	}
-
 }
